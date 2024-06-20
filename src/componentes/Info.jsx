08aa4bd0348364be6a98aa8647b15426c3a24181
Ferchox <fernandotriguero123@gmail.com>
2024-06-20 11:38:23 -0400

    componente info realizado

---
+++ src/componentes/Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Info.css";
+
+function Info() {
+  return (
+    <>
+      <h1>{titulo}</h1>
+      <p>{descripcion}</p>
+    </>
+  );
+}
+
+export default Info;
