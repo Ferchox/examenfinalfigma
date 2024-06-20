3d7fabad2a3b57f3a21cee09e246d1d7d83127db
Ferchox <fernandotriguero123@gmail.com>
2024-06-20 11:39:32 -0400

    componente Ejercicio realizado

---
+++ src/componentes/Ejercicio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Ejercicio.css";
+import Info from "./Info";
+
+function Ejercicio() {
+  return (
+    <div className="ejercicio-container">
+      <img src="" alt="" />
+      <Info />
+    </div>
+  );
+}
+
+export default Ejercicio;
